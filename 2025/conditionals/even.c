469b84321b0529e61437b7baf0587165e9169372
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2025-02-19 11:56:09 +0300

    using if statement to check if a number is even or not

---
+++ 2025/conditionals/even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 9;
+    int rem = num % 2;
+
+    if (rem == 0)
+        printf("Even\n");
+    printf("Yes the number is even\n");
+
+    // printf("Done\n");
+
+    return 0;
+}
