b690cbece018080ec8d3f0c0a1d4478760a43a76
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2025-03-13 10:17:18 +0300

    feat: add program to input and display marks for students using a 2D array

---
+++ 2025/loops/2d_arr.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define S 7
+#define M 4
+
+int main()
+{
+    int s, m;
+
+    // printf("Enter the number of students:");
+    // scanf("%d", &s);
+    // printf("Enter the number of scores:");
+    // scanf("%d", &m);
+
+    // int scores[S][M];
+
+    for (int i = 0; i < S; i++)
+    {
+
+        printf("Student %d\n", i);
+        for (int j = 0; j < M; j++)
+        {
+            printf("Mark %d: \t", j);
+            scanf("%d", &scores[i][j]);
+        }
+        printf("*************\n");
+    }
+    printf("Now printing the entries");
+
+    for (int i = 0; i < S; i++)
+    {
+
+        printf("Student %d\n", i);
+        for (int j = 0; j < M; j++)
+        {
+            printf("Mark %d: %d \n", j, scores[i][j]);
+        }
+        printf("*************\n");
+    }
+    return 0;
+}
