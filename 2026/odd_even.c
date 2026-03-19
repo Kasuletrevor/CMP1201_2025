e5dce12b08ee61ac7bb5876ad7c26a5ebb8f347a
Kasuletrevor <kasulejohntrevor@gmail.com>
2026-03-19 10:06:00 +0300

    19/03/2026

---
+++ 2026/odd_even.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int rem = num % 2;
+
+    if (rem == 1)
+    {
+        printf("%d is odd\n", num);
+    }
+    else
+    {
+        printf("%d is even\n", num);
+    }
+
+    return 0;
+}
