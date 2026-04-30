9e0244d53d9be6badd706bbbaeb7dd2dc18fe19d
Kasuletrevor <kasulejohntrevor@gmail.com>
2026-04-30 17:52:52 +0300

    Refactor formatting and indentation in main.c for improved readability

--- 2024/main.c
+++ 2024/main.c
@@ -4,14 +4,14 @@
 int main()
 {
 
-float area, radius;
-printf("Enter the radius:");
-scanf("%f",&radius);
+    float area, radius;
+    printf("Enter the radius:");
+    scanf("%f", &radius);
 
-area=PI*radius*radius;
+    area = PI * radius * radius;
 
-printf("The area of circle of radius %.2f is %.2f",radius, area);
-// printf("area of the circle is %f", area);
+    printf("The area of circle of radius %.2f is %.2f", radius, area);
+    // printf("area of the circle is %f", area);
 
     return 0;
 }
